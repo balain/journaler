9704200c6dfe1c5cde228926f0d31cb5363bdfc1
John <balain@users.noreply.github.com>
2025-04-25 09:18:51 -0400

    Added lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+#![deny(unsafe_code)]
+#![warn(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::style)]
+#![warn(clippy::explicit_into_iter_loop, clippy::large_stack_arrays, clippy::string_add, clippy::todo)]
+
+// lib.rs for journaler. Add your library code below.
